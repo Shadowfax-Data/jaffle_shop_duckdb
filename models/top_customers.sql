a060a3fa2141d4e1d969ae8374521f3dcf78cad6
sidjha-sf <sid.jha@shadowfaxdata.com>
2025-01-07 16:39:44 -0500

    Revert "docs: Add inline documentation to top_customers model"
    This reverts commit a4820f694cfc7be118b038933ae66cf735067b89.

--- models/top_customers.sql
+++ models/top_customers.sql
@@ -1,13 +1,6 @@
-/*
-    This model identifies and ranks customers based on their order volume.
-    It aggregates the total number of orders placed by each customer,
-    providing a clear view of the most active customers in terms of 
-    order frequency.
-*/
-
 select
-    customer_id,  -- Unique identifier for each customer
-    count(*) as number_of_orders  -- Total number of orders placed by the customer
+    customer_id,
+    count(*) as number_of_orders
 from {{ ref('orders') }}
 group by customer_id
 order by number_of_orders desc
