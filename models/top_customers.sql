5c81409064970103469800f89adcc11448d86068
sidjha-sf <sid.jha@shadowfaxdata.com>
2025-01-07 15:30:57 -0500

    Revert "Add documentation for top_customers model"
    This reverts commit 84aa6fa8afc0d1f8febc3d4fa2452e07e658ac1c.

--- models/top_customers.sql
+++ models/top_customers.sql
@@ -1,15 +1,3 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
-/*
-    This model identifies top customers based on their total number of orders.
-    It provides a simple ranking of customers by order count, which can be used
-    to analyze customer engagement and identify the most active customers.
-*/
-
 select
     customer_id,
     count(*) as number_of_orders
