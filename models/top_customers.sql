03c76b5fd1151a460b45413bc8c7b2779a753756
sidjha-sf <sid.jha@shadowfaxdata.com>
2025-01-08 00:11:26 -0500

    Revert "docs: Add inline documentation to top_customers model"
    This reverts commit ca43ad73676506e5098f95f642a048701ebcd6f3.

--- models/top_customers.sql
+++ models/top_customers.sql
@@ -1,14 +1,3 @@
-{#
-    This model identifies top customers based on their order frequency.
-    It aggregates data from the orders table to calculate the total number
-    of orders placed by each customer.
-
-    Columns:
-    - customer_id: Unique identifier for each customer
-    - number_of_orders: Total count of orders placed by the customer,
-      sorted in descending order to show top customers first
-#}
-
 select
     customer_id,
     count(*) as number_of_orders
